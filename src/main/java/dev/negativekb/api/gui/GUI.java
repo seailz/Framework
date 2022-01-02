2dad00d34346cae64f109b77e597051edf07afef
Negative <60104846+NegativeKB@users.noreply.github.com>
2022-01-02 03:55:24 -0330

    Remove unused import

--- src/main/java/dev/negativekb/api/gui/GUI.java
+++ src/main/java/dev/negativekb/api/gui/GUI.java
@@ -25,7 +25,6 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitTask;
 
 import java.util.ArrayList;
 import java.util.HashMap;
