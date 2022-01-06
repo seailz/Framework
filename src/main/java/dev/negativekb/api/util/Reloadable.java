f4ba884f919dfce10300250072eb43605c31cbf3
Negative <60104846+NegativeKB@users.noreply.github.com>
2022-01-05 23:11:26 -0330

    Add Disableable and Reloadable interfaces

---
+++ src/main/java/dev/negativekb/api/util/Reloadable.java
@@ -0,0 +1,11 @@
+package dev.negativekb.api.util;
+
+/**
+ * Represents a class that will run code when the
+ * {@link Reloadable#reload()} method is run.
+ */
+public interface Reloadable {
+
+    void reload();
+
+}
