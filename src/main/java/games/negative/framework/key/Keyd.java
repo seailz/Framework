2d540be420801f83f15f70a713184a0bbf9f3050
Negative <60104846+NegativeKB@users.noreply.github.com>
2022-04-03 20:13:59 -0230

    Add Keyd interface

---
+++ src/main/java/games/negative/framework/key/Keyd.java
@@ -0,0 +1,24 @@
+package games.negative.framework.key;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a class which needs to be identified by a key.
+ * @param <T> The type of the key.
+ */
+public interface Keyd<T>{
+
+    /**
+     * Gets the key.
+     * @return The key.
+     */
+    @NotNull
+    T getKey();
+
+    /**
+     * Sets the key.
+     * @param key The new key.
+     */
+    void setKey(@NotNull T key);
+
+}
