0e37030793412a3f2ec441b97bbd15094bcdf174
Negative <60104846+NegativeKB@users.noreply.github.com>
2022-04-03 20:22:20 -0230

    Update ShortCommandsProvider.java

--- src/main/java/games/negative/framework/command/shortcommand/provider/ShortCommandsProvider.java
+++ src/main/java/games/negative/framework/command/shortcommand/provider/ShortCommandsProvider.java
@@ -32,7 +32,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
-@SuppressWarnings("all")
 public class ShortCommandsProvider extends ShortCommands {
 
     private final HashMap<Command, ArrayList<String>> commandShortCommands = new HashMap<>();
@@ -47,8 +46,7 @@ public class ShortCommandsProvider extends ShortCommands {
         ArrayList<String> strings = commandShortCommands.getOrDefault(command, new ArrayList<>());
         Arrays.stream(commands).filter(s -> !strings.contains(s)).forEach(strings::add);
 
-        boolean b = commandShortCommands.containsKey(command);
-        if (b)
+        if (commandShortCommands.containsKey(command))
             commandShortCommands.replace(command, strings);
         else
             commandShortCommands.put(command, strings);
@@ -59,8 +57,7 @@ public class ShortCommandsProvider extends ShortCommands {
         ArrayList<String> strings = subCommandShortCommands.getOrDefault(command, new ArrayList<>());
         Arrays.stream(commands).filter(s -> !strings.contains(s)).forEach(strings::add);
 
-        boolean b = subCommandShortCommands.containsKey(command);
-        if (b)
+        if (subCommandShortCommands.containsKey(command))
             subCommandShortCommands.replace(command, strings);
         else
             subCommandShortCommands.put(command, strings);
@@ -68,20 +65,15 @@ public class ShortCommandsProvider extends ShortCommands {
 
     @Override
     public Optional<Command> getCommand(@NotNull String cmd) {
-        Optional<Map.Entry<Command, ArrayList<String>>> entry = commandShortCommands.entrySet()
-                .stream()
-                .filter(commandArrayListEntry -> commandArrayListEntry.getValue().stream().anyMatch(s -> s.equalsIgnoreCase(cmd)))
-                .findFirst();
-        return entry.map(Map.Entry::getKey);
+        return commandShortCommands.entrySet().stream()
+                .filter(entry -> entry.getValue().stream().anyMatch(s -> s.equalsIgnoreCase(cmd)))
+                .map(Map.Entry::getKey).findFirst();
     }
 
     @Override
     public Optional<SubCommand> getSubCommand(@NotNull String cmd) {
-        Optional<Map.Entry<SubCommand, ArrayList<String>>> entry = subCommandShortCommands.entrySet()
-                .stream()
-                .filter(subCommandArrayListEntry -> subCommandArrayListEntry.getValue().stream().anyMatch(s -> s.equalsIgnoreCase(cmd)))
-                .findFirst();
-
-        return entry.map(Map.Entry::getKey);
+        return subCommandShortCommands.entrySet().stream()
+                .filter(entry -> entry.getValue().stream().anyMatch(s -> s.equalsIgnoreCase(cmd)))
+                .map(Map.Entry::getKey).findFirst();
     }
 }
