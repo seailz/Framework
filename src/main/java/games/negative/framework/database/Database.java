590466ada103c12ebaacc3082b88ebe6401148f3
Seailz <81972974+IoyoCode@users.noreply.github.com>
2022-06-09 13:15:44 +0100

    feat: Added back the JDBC url but as deprecated

--- src/main/java/games/negative/framework/database/Database.java
+++ src/main/java/games/negative/framework/database/Database.java
@@ -52,6 +52,7 @@ public class Database {
     private String username;
     private String password;
     private String databaseName;
+    private String url = null;
 
     private Connection connection;
 
@@ -74,15 +75,28 @@ public class Database {
     }
 
     /**
+     * Initiate a {@code Database} using a {@code URL}
+     * @param url A {@link String} which is the value of your {@code JDBC} connection string
+     * @author Seailz
+     */
+    @Deprecated
+    public Database(String url) throws ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        this.url = url.toString();
+    }
+
+
+    /**
      * Initiate the connection to the database
      * @author Seailz
      */
     public void connect() throws SQLException {
-            connection = DriverManager.getConnection(
+            connection = url == null ? DriverManager.getConnection(
                     "jdbc:mysql://" + getIp() + ":" + getPort() + "/" + getDatabaseName(),
                     getUsername(),
                     getPassword()
-            );
+            ) : DriverManager.getConnection(url);
     }
 
     public void disconnect() throws SQLException {
