199973d7f7db6d328b010d02c0e2b363ed662a48
IoyoCode <81972974+IoyoCode@users.noreply.github.com>
2022-07-02 17:04:29 +0100

    fix: updated more stuff, hopefully it works :/

--- src/main/java/games/negative/framework/gui/sign/SignManager.java
+++ src/main/java/games/negative/framework/gui/sign/SignManager.java
@@ -8,6 +8,7 @@ import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.reflect.StructureModifier;
 import com.comphenix.protocol.wrappers.BlockPosition;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import com.comphenix.protocol.wrappers.nbt.NbtCompound;
 import games.negative.framework.BasePlugin;
 import games.negative.framework.gui.sign.packets.SignEditorPacket;
@@ -97,8 +98,10 @@ public class SignManager {
                     SignGUI signGUI = input.get(p);
                     StructureModifier<String> values = event.getPacket().getStrings();
 
-                    String[] lines = new String[]{values.read(1), values.read(2), values.read(3), values.read(4)};
-                    signGUI.getOnClose().accept(p, lines);
+                    // String[] lines = new String[]{values.read(1), values.read(2), values.read(3), values.read(4)};
+                    WrappedChatComponent[] lines = event.getPacket().getChatComponentArrays().read(0);
+                    String[] linesStrings = new String[]{lines[0].toString(), lines[1].toString(), lines[2].toString(), lines[3].toString()};
+                    signGUI.getOnClose().accept(p, linesStrings);
                     input.remove(p);
                 }
             }
